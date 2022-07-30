9dec87c698b5bd3e02103e880ce3c25193e15d2c
Taisuke Fukuno <fukuno@jig.jp>
2022-07-31 08:24:54 +0900

    Initial commit

---
+++ Camera.js
@@ -0,0 +1,30 @@
+export class Camera {
+  constructor(videoElement, opt) {
+    this.videoElement = videoElement;
+    this.opt = opt;
+  }
+  async start() {
+    const devs = await navigator.mediaDevices.enumerateDevices();
+    //console.log(devs);
+    const dev = devs.find(d => d.kind == "videoinput" && d.label.toLowerCase().indexOf("virtual") == -1);
+    //console.log(dev);
+    const opt = {
+      video: {
+        deviceId: dev.deviceId,
+        width: this.opt.width || 1280,
+        height: this.opt.height || 720,
+      },
+    };
+    const stream = await navigator.mediaDevices.getUserMedia(opt);
+    //console.log(stream);
+    this.videoElement.srcObject = stream;
+    this.delay = 1000 / (opt.fps || 30);
+
+    this.videoElement.play();
+    const f = async () => {
+      await this.opt.onFrame();
+      setTimeout(f, this.delay);
+    };
+    f();
+  }
+};
