c171bad3ee47410e881e2d2b2bbd5ef567fb5c56
Taisuke Fukuno <fukuno@jig.jp>
2022-09-08 07:56:28 +0900

    fix use camera

--- Camera.js
+++ Camera.js
@@ -4,18 +4,16 @@ export class Camera {
     this.opt = opt;
   }
   async start() {
-    const devs = await navigator.mediaDevices.enumerateDevices();
-    //console.log(devs);
-    const dev = devs.find(d => d.kind == "videoinput" && d.label.toLowerCase().indexOf("camera") >= 0 && d.label.toLowerCase().indexOf("virtual") == -1);
-    //console.log(dev);
-    const opt = {
-      video: {
-        deviceId: dev?.deviceId,
-        width: this.opt.width || 1280,
-        height: this.opt.height || 720,
-      },
+    const video = {
+      width: this.opt.width || 1280,
+      height: this.opt.height || 720,
     };
-    const stream = await navigator.mediaDevices.getUserMedia(opt);
+    const devs = await navigator.mediaDevices.enumerateDevices();
+    const dev = devs.find(d => d.kind == "videoinput" && d.label.toLowerCase().indexOf("camera") >= 0 && d.label.toLowerCase().indexOf("virtual") == -1);
+    if (dev) {
+      video.deviceId = dev.deviceId;
+    }
+    const stream = await navigator.mediaDevices.getUserMedia({ video });
     //console.log(stream);
     this.videoElement.srcObject = stream;
     this.delay = 1000 / (opt.fps || 30);
